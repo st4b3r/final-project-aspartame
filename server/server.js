2054721b7604052a56c1cc7443fc12ef44946531
Christian <christian.tegge@posteo.org>
2022-06-02 12:20:53 +0200

    setting up for Heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const compression = require("compression");
 const path = require("path");
+let sessionSecret = process.env.SESSION_SECRET;
+
+if (!sessionSecret) {
+    sessionSecret = require("../secrets.json").SESSION_SECRET;
+}
+if (process.env.NODE_ENV == "production") {
+    app.use((req, res, next) => {
+        if (req.headers["x-forwarded-proto"].startsWith("https")) {
+            return next();
+        }
+        res.redirect(`https://${req.hostname}${req.url}`);
+    });
+}
 
 app.use(compression());
 
